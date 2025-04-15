c0e75b1f6eb46cb2f31d4122700eb57a3a4b9bf6
Bruno Policarpo <bruno.freitas@cefet-rj.br>
2025-04-14 21:21:12 -0300

    Updating grade_student.sh

--- .grade_student.sh
+++ .grade_student.sh
@@ -1,27 +1,48 @@
 #!/bin/bash
 
 points=0
+total=0
 
-./questao01.sh 
-
+echo "QUESTAO 1"
+./questao01.sh
+./.tests/criacao_pastas.sh
 [[ $? -eq 0 ]] && points=$(( points + 1 ))
+total=$(( total + 1))
+echo
 
-./questao02.sh 
-
+echo "QUESTAO 2"
+./questao02.sh
+./.tests/copiar_systemd_para_joao.sh
 [[ $? -eq 0 ]] && points=$(( points + 1 ))
+total=$(( total + 1))
+echo
 
-./questao03.sh 
-
+echo "QUESTAO 3"
+./questao03.sh
+./.tests/copiar_passwd_group_para_maria.sh
 [[ $? -eq 0 ]] && points=$(( points + 1 ))
+total=$(( total + 1))
+echo
 
-./questao04.sh 
-
+echo "QUESTAO 4"
+./questao04.sh
+./.tests/criar_copias_joao_maria.sh
 [[ $? -eq 0 ]] && points=$(( points + 1 ))
+total=$(( total + 1))
+echo 
 
-./questao05.sh 
-
+echo "QUESTAO 5"
+./questao05.sh
+./.tests/criar_mais_uma_pasta.sh
 [[ $? -eq 0 ]] && points=$(( points + 1 ))
+total=$(( total + 1))
+echo 
 
-./questao06.sh 
-
+echo "QUESTAO 6"
+./questao06.sh
+./.tests/renomear_em_mais_uma_pasta.sh
 [[ $? -eq 0 ]] && points=$(( points + 1 ))
+total=$(( total + 1))
+echo
+
+echo "NOTA: ${points}/${total}"
